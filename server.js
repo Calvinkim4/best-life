5d5b121af756ef385050c8672532a448e2205eaf
Calvin Kim <calvinkim4@gmail.com>
2019-05-13 14:28:52 -0400

    created server file and created endpoint routers

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const { userRouter } = require('./routes/sserRouter');
+const { entriesRouter } = require('./routes/entriesRouter');
+const { foodRouter } = require('./routes/foodRouter');
+const { exerciseRouter } = require('./routes/exerciseRouter');
+
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3015;
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/user', userRouter);
+
+app.use('/entries', entriesRouter);
+
+app.use('/food', foodRouter);
+
+app.use('/exercise', exerciseRouter);
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
