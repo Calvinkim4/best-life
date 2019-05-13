c2a8180a46cec7392368e3cb3f31cd77acabc4d2
larsthrasher <thrasherlars@gmail.com>
2019-05-13 15:50:11 -0400

    create and update routes for foodrouter

--- routes/foodRouter.js
+++ routes/foodRouter.js
@@ -1,3 +1,47 @@
 const express = require('express');
 const { Food } = require('../models')
 const foodRouter = express.Router();
+
+// all foods with entry id
+foodRouter.get('/:id', async (request, response) =>
+{
+  try {
+    const id = request.params.id;
+    const food = await Food.findAll({
+      where: {
+        entry_id: id,
+      }
+    });
+    response.json({food})
+  } catch (e) {
+    response.status(500).json({ msg: e.message })
+  }
+})
+
+// create food entry
+foodRouter.post('/', async (request, response) => {
+  try {
+    const createFood = await Food.create(request.body)
+    response.json({
+      createFood
+    })
+  } catch (e) {
+    response.status(500).json({ msg: e.message })
+  }
+})
+//update food entry
+foodRouter.put('/:id', async (request, response) => {
+  try {
+    const id = request.params.id;
+    const updateFood = await Food.findByPk(id);
+
+    if (updateFood) await updateFood.update(request.body);
+    response.json({
+      updateFood
+    });
+  } catch(e) {
+    response.status(304).json({
+      message: e.message
+    });
+  }
+})
