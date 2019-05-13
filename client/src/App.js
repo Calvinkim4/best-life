1529f11d7c64e9682802e81963d470736f034b58
Loriana Hines <lorianahines@gmail.com>
2019-05-13 16:44:35 -0400

    added dashboard and login path

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import './App.css';
-import LoginForm from './components/LoginForm/LoginForm'
+import axios from 'axios';
+import { Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>BestLife</h1>
-      <LoginForm />
+//components
+import LoginForm from './components/LoginForm/LoginForm';
+import Dashboard from './components/Dashboard/Dashboard'
 
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      signedIn: false
+    }
+  }
+
+  handleSignIn = () =>{
+    this.setState({
+      signedIn: true
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>BestLife</h1>
+        { !this.state.signedIn && <LoginForm signIn = {this.handleSignIn} />}
+        { this.state.signedIn && <Dashboard/>}
+
+        
+
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './LoginForm.css'
 
 class LoginForm extends React.Component{
   render(){
     return(
-      <form className='login-form'>
-        <label htmlFor='username'>Username</label>
-        <input type='text' name='username' />
-        <label htmlFor='password'>Password</label>
-        <input type='password' name='password' />
-        <button type='submit'>Login</button>
+      <form className='login-form' onSubmit={this.props.signIn}>
+        <label htmlFor='username'>
+          Username
+          <input type='text' name='username' />
+        </label>
+        <label htmlFor='password'>
+          Password
+          <input type='password' name='password' />
+        </label>
+        <button className='login-btn'type='submit'>Login</button>
       </form>
     )
   }
