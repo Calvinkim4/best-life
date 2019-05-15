7f41309fc603a95036f7fd9699fecf95704f903a
Loriana Hines <lorianahines@gmail.com>
2019-05-15 14:15:47 -0400

    can update user info

--- client/src/components/Stats/Stats.js
+++ client/src/components/Stats/Stats.js
@@ -7,7 +7,9 @@ class Stats extends React.Component{
   constructor(){
     super();
     this.state={
-      user: []
+      user: [],
+      updatedUser: {},
+      show: false
     }
   }
   
@@ -16,10 +18,78 @@ class Stats extends React.Component{
     this.setState({
       user: user
     });
+  };
 
+  showModal = async (event) => {
+    const id = 1;
+    const user = await getUser(id);
+
+    this.setState({ 
+      show: true,
+      updatedUser: user,
+      id: id
+    });
+  };
+
+  hideModal = () => {
+    this.setState({ 
+      show: false 
+    });
+  };
+
+  onUserFormChange = (event) => {
+    const element = event.target
+    const name = element.name
+    let value = element.value
+
+    if (name === 'current_weight' ||  name === 'goal_weight' || name === 'calorie_intake' ) {
+        value = parseInt(value);
+    }
+
+    const newState = {}
+    newState[name] = value
+
+    this.setState(newState)
   }
 
+  onUpdateClick = async (event) => {
+    event.preventDefault();
+    const id = this.state.id;
+
+    let updatedUser= {
+      current_weight: this.state.current_weight,
+      goal_weight: this.state.goal_weight,
+      calorie_intake: this.state.calorie_intake
+    }
+
+    await updateUser(id, updatedUser);
+    const user = await getUser(id);
+      this.setState({
+        user: user
+      })
+    this.hideModal();
+  }
+
+
   render(){
+    const modal = (this.state.show && this.state.updatedUser) ? 
+            <div className='modal'>
+              <section className="modal-content">
+              <form onSubmit={this.onUpdateClick}>
+                <label htmlFor='current-weight'>
+                  <input type='text' name='current_weight' placeholder={this.state.updatedUser.current_weight} onChange={ this.onUserFormChange }/>
+                </label>
+                <label htmlFor='goal-weight'>
+                  <input type='text' name='goal_weight' placeholder={this.state.updatedUser.goal_weight}  onChange={ this.onUserFormChange }/>
+                </label>
+                <label htmlFor='calorie-limit'>
+                  <input type='text' name='calorie_limit' placeholder={this.state.updatedUser.calorie_intake}  onChange={ this.onUserFormChange }/>
+                </label>
+                <button type='submit'>Update Stats</button>
+              </form>
+                <button onClick={this.hideModal}>Close</button>
+              </section>
+            </div> : null;
     return(
       <div className='stats'>
         <h1>My Stats</h1>
@@ -27,8 +97,9 @@ class Stats extends React.Component{
           <h3>Current Weight: {this.state.user.current_weight} lbs</h3>
           <h3>Goal Weight: {this.state.user.goal_weight} lbs</h3>
           <h3>Calorie Limit: {this.state.user.calorie_intake}</h3> 
-          <button>Update</button>
+          <button onClick={this.showModal}>Update</button>
         </div>
+        {modal}
       </div>
     )
   }
